e90d47e04a9f68c336eb93a516aea74648c42ffa
Semhar Haile <semharh24@gmail.com>
2023-06-24 23:30:51 +0000

    added code to convert data to df

--- youtube.py
+++ youtube.py
@@ -1,14 +1,15 @@
 import os
 import json
 import requests
-
+import sqlalchemy as db
+import pandas as pd
 
 API_KEY = os.environ.get('YOUTUBEAPI')
 BASE_URL = "https://www.googleapis.com/youtube/v3"
 country_code = input("Enter country code (ex. US, CA): ")
 part = "snippet,contentDetails,statistics"
 
-url = f"{BASE_URL}/videos?part={part}&chart=mostPopular&regionCode={country_code}&maxResults=3&key={API_KEY}"
+url = f"{BASE_URL}/videos?part={part}&chart=mostPopular&regionCode={country_code}&maxResults=10&key={API_KEY}"
 
 response = requests.get(url)
 
@@ -20,9 +21,20 @@ print(f"""
 
 Popular Videos in {country_code}:""")
 
+youtubevideos = pd.DataFrame({
+    "Title": [],
+    "Description": [],
+    "Channel": [],
+    "Tags": [],
+    "Views": [],
+    "Likes Count": [],
+    "Comments Count": []})
+
+
 for video in videos:
     title = video["snippet"]["title"]
-    description = video["snippet"]["description"][:200]
+
+    description = video["snippet"]["description"][:200] + ".."
     channel = video["snippet"]["channelTitle"]
      
     if "tags" in video["snippet"]:
@@ -33,13 +45,26 @@ for video in videos:
     views = video["statistics"]["viewCount"]
     likes = video["statistics"]["likeCount"]
     comments = video["statistics"]["commentCount"]
+    
+    row = {"Title": title, "Description": description,
+      "Channel": channel, "Tags": str(tags),
+      "Views": views, "Likes Count": likes,
+      "Comments Count": comments}
+   
+    youtubevideos.loc[len(youtubevideos)] = row
     print(
       f"""
       Title: {title}
-      Description: {description}...
+      Description: {description}
       Channel: {channel}
       Tags: {tags}
       Views: {views}
       Likes Count: {likes}
       Comments Count: {comments}
       """)
+
+engine = db.create_engine('sqlite:///youtubevideos.db')
+youtubevideos.to_sql("popular", con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM popular;")).fetchall()
+   print(pd.DataFrame(query_result))
